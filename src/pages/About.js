ade1e19ccea29da61b6bbdea16dc7ec6743aec85
Atharva Ashar <atharvavashar5@gmail.com>
2022-09-15 22:47:16 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ratione
+        corporis iure quae. Pariatur, ipsam esse sequi magnam neque tempore sed
+        modi nesciunt earum atque id numquam quod magni eos incidunt fugiat
+        voluptas voluptate ullam aspernatur rem nam voluptatum quibusdam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
